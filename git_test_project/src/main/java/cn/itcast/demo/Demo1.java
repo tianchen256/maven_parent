553f3107333e1c89c6e1d08f9ec92ae41b4f52d5
tianchen256 <478823577@qq.com>
2019-06-12 16:34:34 +0800

    提交初始代码

---
+++ git_test_project/src/main/java/cn/itcast/demo/Demo1.java
@@ -0,0 +1,13 @@
+package cn.itcast.demo;
+
+/**
+ * @author tianchen
+ * @date 2019-06-12 16:31
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
+    }
+}
